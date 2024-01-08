dd8ec0facb4704800f4707f3122ea1f2bdc54fe5
Jupyo Hong <jupyohong7@gmail.com>
2024-01-08 23:32:56 +0900

    파일 시스템을 사용하여 주소록 기능 추가

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # 한라산탐방 예약시스템 인원체크 프로그램
 
 산 한 번 가는게 왜 이리 힘든지...
+
+## How to start
+
+0. Git Clone
+
+```bash
+cd
+git clone git@github.com:ohdnf/visit-halla.git
+cd visit-halla/
+npm ci --omit=dev
+```
+
+1. `.env` 파일 만들기
+
+```bash
+cp .env.sample .env
+vi .env # 내용 채우기
+```
+
+2. `address_book.csv` 파일 만들기
+
+```bash
+echo '${TELEGRAM_CHAT_ID},null' >> address_book.csv
+```
+
+3. cron 설정
+
+```bash
+crontab -e
+```
+
+```bash
+* * * * * .nvm/versions/node/v20.10.0/bin/node ~/visit-halla/index.js >> ~/logfile.log 2>&1
+```
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const axios = require('axios');
+const fs = require('fs/promises');
 
 (async () => {
   try {
-    // 예약인원 조회
+    // 예약 인원 조회
     const formData = new FormData();
     formData.append('courseSeq', process.env.COURSE_SEQ);
     formData.append('visitDt', process.env.VISIT_DATE);
@@ -21,19 +22,46 @@ const axios = require('axios');
     }
     const coursePerson = jejuResponse.data.coursePerson;
     if (!coursePerson) return;
-    const reserveCnt = +coursePerson.reserveCnt;
-    const limitCnt = +coursePerson.limitCnt;
-    if (reserveCnt >= limitCnt) return;
-    // 예약 정원이 비었을 때 텔레그램 봇으로 나에게 메시지 보내기
-    await axios({
-      method: 'get',
-      url: process.env.TELEGRAM_BOT_API_URL,
-      data: {
-        chat_id: process.env.TELEGRAM_CHAT_ID,
-        text: `현재예약인원 ${reserveCnt}/${limitCnt}
-${process.env.VISIT_HALLA_MAIN_URL}`,
-      },
+    const reserveCnt = +coursePerson.reserveCnt; // 현재 예약 인원
+    const limitCnt = +coursePerson.limitCnt; // 총 예약 가능 인원
+    // 주소록 관리
+    const fsData = await fs.readFile('./address_book.csv', {
+      encoding: 'utf-8',
     });
+    const addressBook = fsData.split('\n').map((e) => e.split(','));
+    if (!addressBook?.length) return; // 메시지 보낼 사람이 없음
+    if (reserveCnt >= limitCnt) return; // 정원 초과
+    // 예약 정원이 비었을 때 텔레그램 봇으로 예약 가능 메시지 보내기
+    const lastSentList = []; // 마지막으로 보낸 일시 업데이트 배열
+    for (const user of addressBook) {
+      const [chatId, msString] = user; // 텔레그램 ID, 마지막으로 보낸 시간(ms)
+      if (msString !== 'null') {
+        // 이전에 전송한 이력이 존재할 경우
+        const ms = new Date(+msString).getTime();
+        if (Date.now() - ms > 1000 * 60 * 60) {
+          // 보낸지 1시간이 넘은 경우만 새로 전송
+          lastSentList.push(`${chatId},null`);
+        } else {
+          // 1시간 이내일 경우 메시지 전송 보류
+          lastSentList.push(`${chatId},${ms}`);
+          continue;
+        }
+      } else {
+        // 처음 보내는 경우
+        lastSentList.push(`${chatId},${Date.now()}`);
+      }
+      await axios({
+        method: 'get',
+        url: `${process.env.TELEGRAM_BOT_API_URL}/sendMessage`,
+        data: {
+          chat_id: chatId,
+          text: `현재예약인원 ${reserveCnt}/${limitCnt}
+  ${process.env.VISIT_HALLA_MAIN_URL}`,
+        },
+      });
+    }
+    // 주소록 업데이트
+    await fs.writeFile('./address_book.csv', lastSentList.join('\n'));
   } catch (err) {
     console.error(err);
   }
